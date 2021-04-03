1088682f5f0661b33b2b4b7aec347b4c7631c54b
Vikas Pandey <vipandey54@gmail.com>
2021-04-03 13:50:03 +0530

    initial commit

---
+++ TheameManager/TheameManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheameManager
+//
+//  Created by DevilStiffer on 03/04/21.
+//  Copyright © 2021 xyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
